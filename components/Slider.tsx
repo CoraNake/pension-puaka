b5d8127f2876fcc22ecac1fd7bc88526d13f905b
CoraNake <118303388+CoraNake@users.noreply.github.com>
2022-11-25 12:51:33 -1000

    1 file modified

--- components/Slider.tsx
+++ components/Slider.tsx
@@ -41,7 +41,7 @@ export default function App() {
         <SwiperSlide>
           <img
             src={slide.url}
-            style={{ height: '30rem', objectFit: 'cover' }}
+            style={{ height: '30rem', minWidht: '100%', objectFit: 'cover' }}
           />
         </SwiperSlide>
       ))}
