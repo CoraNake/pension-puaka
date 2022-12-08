1f2fc48d2eb79922e7fab20e6d13f99adf681d44
CoraNake <118303388+CoraNake@users.noreply.github.com>
2022-12-08 08:43:36 -1000

    4 files modified

--- App.tsx
+++ App.tsx
@@ -1,40 +1,19 @@
 import * as React from 'react';
-import './style.css';
-import Slider from './components/Slider';
 import Navbar from './components/Navbar';
+import Slider from './components/Slider';
+import Accueil from './pages/Accueil';
 
-import { Activites } from './pages/Activites';
-import { ChambresEtBungalows } from './pages/ChambresEtBungalows';
-import { Evenements } from './pages/Evenements';
-import { Locations } from './pages/Locations';
-import { Formules } from './pages/Formules';
-import { Accueil } from './pages/Accueil';
-import { Reservations } from './pages/Reservations';
-import { Contact } from './pages/Contact';
-
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Footer from './components/Footer';
 
+import './style.css';
+
 export default function App() {
-  const router = createBrowserRouter([
-    { path: '/', element: <Accueil /> },
-
-    { path: '/activites', element: <Activites /> },
-    { path: '/chambresetbungalows', element: <ChambresEtBungalows /> },
-    { path: '/locations', element: <Locations /> },
-    { path: '/evenements', element: <Evenements /> },
-    { path: '/formules', element: <Formules /> },
-    { path: '/reservations', element: <Reservations /> },
-    { path: '/contact', element: <Contact /> },
-  ]);
-
   return (
-    <React.Fragment>
+    <div>
       <Navbar />
       <Slider />
-      <RouterProvider router={router} />
-
+      <Accueil />
       <Footer />
-    </React.Fragment>
+    </div>
   );
 }
--- index.tsx
+++ index.tsx
@@ -1,14 +1,34 @@
 import * as React from 'react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import App from './App';
 
+import { Activites } from './pages/Activites';
+import { ChambresEtBungalows } from './pages/ChambresEtBungalows';
+import { Evenements } from './pages/Evenements';
+import { Locations } from './pages/Locations';
+import { Formules } from './pages/Formules';
+import { Accueil } from './pages/Accueil';
+import { Reservations } from './pages/Reservations';
+import { Contact } from './pages/Contact';
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
+const router = createBrowserRouter([
+  { path: '/', element: <Accueil /> },
+
+  { path: '/activites', element: <Activites /> },
+  { path: '/chambresetbungalows', element: <ChambresEtBungalows /> },
+  { path: '/locations', element: <Locations /> },
+  { path: '/evenements', element: <Evenements /> },
+  { path: '/formules', element: <Formules /> },
+  { path: '/reservations', element: <Reservations /> },
+  { path: '/contact', element: <Contact /> },
+]);
 
 root.render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
